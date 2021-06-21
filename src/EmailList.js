c9bc732728708a376068534b8f0a54605d05033b
annpermi <perminova.anna85@gmail.com>
2021-06-21 01:12:57 -0400

    EmailRow

--- src/EmailList.js
+++ src/EmailList.js
@@ -3,6 +3,15 @@ import { Checkbox, IconButton } from '@material-ui/core';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import RedoIcon from '@material-ui/icons/Redo';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import SettingsIcon from '@material-ui/icons/Settings';
+import InboxIcon from '@material-ui/icons/Inbox';
+import PeopleIcon from '@material-ui/icons/People';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import Section from './Section';
+import EmailRow from './EmailRow';
 import './EmailList.css'
 
 function EmailList() {
@@ -21,6 +30,41 @@ function EmailList() {
                         <MoreVertIcon />
                     </IconButton>
                 </div>
+                <div className="emailList__settingsRight">
+                    <IconButton>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChevronRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="emailList__sections">
+                    <Section Icon={InboxIcon} title='primary' color='red' selected/>
+                    <Section Icon={PeopleIcon} title='Social' color='#1A73E8'/>
+                    <Section Icon={LocalOfferIcon} title='Promotions' color='green'/>
+            </div>
+
+            <div className="emailList__list">
+                <EmailRow 
+                title='Gmail'
+                subject='Hey check this out'
+                description='This is a gmail clone'
+                time='10pm'
+                />
+                <EmailRow 
+                title='Facebook'
+                subject='How are you'
+                description='Like please'
+                time='10am'
+                />
             </div>
         </div>
     )
