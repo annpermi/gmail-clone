acce121c3b3cd13080db4623996c776d0f877301
annpermi <perminova.anna85@gmail.com>
2021-06-21 12:36:50 -0400

    mailSlice

--- src/features/mailSlice.js
+++ src/features/mailSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 
-export const counterSlice = createSlice({
-  name: 'counter',
+export const mailSlice = createSlice({
+  name: 'mail',
   initialState: {
     value: 0,
   },
@@ -26,19 +26,6 @@ export const counterSlice = createSlice({
 });
 
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const selectMail = (state) => state.mail.value;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state) => state.counter.value;
-
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
-export const incrementIfOdd = (amount) => (dispatch, getState) => {
-  const currentValue = selectCount(getState());
-  if (currentValue % 2 === 1) {
-    dispatch(incrementByAmount(amount));
-  }
-};
-
-export default counterSlice.reducer;
+export default mailSlice.reducer;
